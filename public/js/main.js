a1319a17fbce8b4f0de015e77a4e3d517d11ba11
arthur <arhurdeveloper@gmail.com>
2021-10-11 19:35:25 -0300

    melhorado quase todos os comportamentos do jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,54 @@
-const num = $('p.frase').text().split(' ').length
-
-$('#qtdPalavras').text(num)
-
+const tempoInicial = $('#tempo').text()
 const area = $('textarea')
 
-area.on('input', function() {
+$(function() {
+	areaEvent()
+	timeEvent()
+	buttonEvent()
+})
 
-	let conteudo = area.val()
-	let qtd = conteudo.trim().split(/\S+/).length - 1
-
-	$('#contadorCaracter').text(conteudo.trim().length)
-	$('#contadorPalavra').text(qtd)
+function areaEvent() {
+	const num = $('p.frase').text().split(' ').length
+	$('#qtdPalavras').text(num)
 
 	
-})
+
+	area.on('input', function() {
+
+		let conteudo = area.val()
+		let qtd = conteudo.trim().split(/\S+/).length - 1
+	
+		$('#contadorCaracter').text(conteudo.trim().length)
+		$('#contadorPalavra').text(qtd)
+	})
+}
+
+function timeEvent() {
+	const tempo = $('#tempo')
+	let count = parseInt(tempo.text())
+	area.one('focus', function() {
+		$('button').attr('disabled', true)
+		const id = setInterval(() => {
+			count--
+
+			tempo.text(count)
+			if(count < 1) {
+				area.attr('disabled', true)
+				clearInterval(id)
+
+				$('#contadorCaracter').text(0)
+				$('#contadorPalavra').text(0)
+				$('button').attr('disabled', false)
+			}
+		}, 1000)
+	})
+}
+
+function buttonEvent() {
+	$('button').click(() => {
+		area.attr('disabled', false)
+		area.val("")
+		$('#tempo').text(tempoInicial)
+		timeEvent()
+	})
+}
