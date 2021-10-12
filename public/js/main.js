e183ee6067b600cb9470e546a4a7c2f77c1eae83
arthur <arhurdeveloper@gmail.com>
2021-10-11 22:44:00 -0300

    terminado os eventos para o jogo funcinar

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 const tempoInicial = $('#tempo').text()
 const area = $('textarea')
-
+const frase = $('.frase').text()
 $(function() {
 	areaEvent()
 	timeEvent()
@@ -11,15 +11,23 @@ function areaEvent() {
 	const num = $('p.frase').text().split(' ').length
 	$('#qtdPalavras').text(num)
 
-	
-
 	area.on('input', function() {
 
 		let conteudo = area.val()
 		let qtd = conteudo.trim().split(/\S+/).length - 1
-	
+		let comparavel = frase.substr(0, conteudo.length)
+
+		console.log(conteudo)
 		$('#contadorCaracter').text(conteudo.trim().length)
 		$('#contadorPalavra').text(qtd)
+
+		console.log(comparavel, conteudo)
+		if(comparavel == conteudo){
+			area.removeClass('campo-errado')
+			area.addClass('campo-correto')
+		}else {
+			area.addClass('campo-errado')
+		}
 	})
 }
 
@@ -27,7 +35,7 @@ function timeEvent() {
 	const tempo = $('#tempo')
 	let count = parseInt(tempo.text())
 	area.one('focus', function() {
-		$('button').attr('disabled', true)
+		$('#reset').attr('disabled', true)
 		const id = setInterval(() => {
 			count--
 
@@ -36,16 +44,22 @@ function timeEvent() {
 				area.attr('disabled', true)
 				clearInterval(id)
 
-				$('#contadorCaracter').text(0)
-				$('#contadorPalavra').text(0)
-				$('button').attr('disabled', false)
+				$('#reset').attr('disabled', false)
+				area.addClass('campo-desativado')
 			}
 		}, 1000)
 	})
 }
 
 function buttonEvent() {
-	$('button').click(() => {
+	$('#reset').click(() => {
+		$('#contadorCaracter').text(0)
+		$('#contadorPalavra').text(0)
+
+		area.removeClass('campo-errado')
+		area.removeClass('campo-correto')
+
+		area.removeClass('campo-desativado')
 		area.attr('disabled', false)
 		area.val("")
 		$('#tempo').text(tempoInicial)
